c0f72fdaea5ba896dccd0c83c707bba6c70169db
Sui Yee <yeeng2001@gmail.com>
2024-03-07 23:42:22 +0800

    login server

--- backend/server.js
+++ backend/server.js
@@ -12,21 +12,22 @@ const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "user"
+    database: "myhalalchecker"
 })
 
 // write login error 
 app.post('/login',[ check('email', "Emaill length error").isEmail().isLength({min: 10, max:30}),    
 check('password', "password length 8-10").isLength({min: 1, max: 10})], 
-(req, res) => {    const sql = "SELECT * FROM login WHERE email = ? AND password = ?";    
+(req, res) => {    const sql = "SELECT * FROM user WHERE email = ? AND password = ?";    
 // get login result (successful or failed)
 db.query(sql, [req.body.email,req.body.password ], 
     (err, data) => {
-        const errors = validationResult(req);        
+        const errors = validationResult(req);       
         if(!errors.isEmpty()) {           
              return res.json(errors);        
         } else {            
-            if(err) {                
+            if(err) {  
+                console.log(err)              
                 return res.json("Error");            
             }            
             if(data.length > 0) {               

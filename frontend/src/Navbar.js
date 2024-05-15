4394eb232b423394aa89413f5b46c6f31bf42d85
Sui Yee <yeeng2001@gmail.com>
2024-05-15 21:36:42 +0800

    login page and nav bar update

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -2,43 +2,48 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useUser } from './UserContext'; // Adjust the path as necessary
 import './Navbar.css';
+import logo from "./images/logo.png";
 
 const Navbar = () => {
-    const { user, setUser, loading } = useUser(); // Ensure loading state is provided from context
+    const { user, setUser, loading } = useUser();
     const [showDropdown, setShowDropdown] = useState(false);
     const navigate = useNavigate();
 
-    // Function to handle logout
     const handleLogout = () => {
         localStorage.removeItem('UserID');
         localStorage.removeItem('username');
-        setUser(null); // Clear user context, adjust based on your context implementation
-        navigate('/'); // Redirect to login page, adjust the route as needed
+        setUser(null);
+        navigate('/');
     };
 
-    // Toggle dropdown
     const toggleDropdown = () => setShowDropdown(!showDropdown);
 
-    // Wait for loading to finish before rendering
     if (loading) {
-      return null; // You can return a loading indicator here if needed
+        return null; // Optionally, return a loading spinner here
     }
 
     return (
         <nav className="navbar">
-            <h4>myHalal Checker</h4>
+            <div className="logocontainer">
+                <img src={logo} alt="myHalal Checker Logo" className="logo1" />
+                <h4>myHalal Checker</h4>
+            </div>
             <div className="links">
                 <Link to="/home">Home</Link>
                 <Link to="/report">Report</Link>
                 <Link to="/enquiry">Enquiry</Link>
                 <Link to="/data">Data</Link>
                 {user && (
-                    <div className="user-menu" onClick={toggleDropdown}>
-                        {user.username}
-                        <div className={`dropdown ${showDropdown ? 'dropdown-active' : ''}`}>
-                            <Link to="/profile">Profile</Link>
-                            <button onClick={handleLogout}>Log out</button>
+                    <div>
+                        <div className="user-menu" onClick={toggleDropdown}>
+                            {user.username}
                         </div>
+                        {showDropdown && (
+                            <div className="dropdown">
+                                <Link to="/profile">Profile</Link>
+                                <button onClick={handleLogout}>Log out</button>
+                            </div>
+                        )}
                     </div>
                 )}
             </div>
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,57 +1,44 @@
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import { useUser } from '../UserContext'; // Make sure this path is correct
+import { useUser } from '../UserContext';
 import Validation from "./LoginValidation";
 import axios from 'axios';
 import '../cssFolder/login.css';
-import court from '../images/court.png';
-import {FaEnvelope, FaLock} from 'react-icons/fa';
+import logo from "../images/logo.png"; 
+import { FaEnvelope, FaLock } from 'react-icons/fa';
 
 function Login() {
-    const [values, setValues] = useState({
-        email: '',
-        password: ''
-    });
+    const [values, setValues] = useState({ email: '', password: '' });
     const navigate = useNavigate();
     const { setUser } = useUser();
     const [errors, setErrors] = useState({});
     const [backendError, setBackendError] = useState([]);
 
-    const handleInput = (event) => {
-        setValues({
-            ...values,
-            [event.target.name]: event.target.value
-        });
+    const handleInput = event => {
+        setValues({ ...values, [event.target.name]: event.target.value });
     };
 
-    const handleSubmit = (event) => {
-        console.log(values)
+    const handleSubmit = event => {
+        console.log('submir')
         event.preventDefault();
         const validationErrors = Validation(values);
-        console.log(validationErrors);
-        setErrors(validationErrors);
         console.log(Object.keys(validationErrors).length)
-        
-        // Proceed if there are no validation errors
+        setErrors(validationErrors);
+
         if (Object.keys(validationErrors).length === 2) {
+            console.log('enter')
             axios.post('http://localhost:8085/login', values)
             .then(res => {
-                console.log(res.data);
                 if (res.data.status === "Success") {
-                    // Set user information upon successful login
-                    setUser({ username: res.data.username }); // Use the returned username
-                    
-                    localStorage.setItem('userID', res.data.userID); // Assuming userID is returned from the server
-                    localStorage.setItem('username', res.data.username)
-                    localStorage.setItem('role', res.data.role)
-                    // Navigate to the home page
+                    setUser({ username: res.data.username });
+                    localStorage.setItem('userID', res.data.userID);
+                    localStorage.setItem('username', res.data.username);
+                    localStorage.setItem('role', res.data.role);
                     navigate('/home');
                 } else {
-                    // Handle different or more specific backend errors as needed
-                    setBackendError([res.data.message || "Invalid email"]); // Use the error message from the backend
+                    setBackendError([res.data.message || "Invalid login attempt"]);
                 }
             })
-            
             .catch(err => {
                 console.error('Error:', err);
                 setBackendError(["An error occurred during login"]);
@@ -60,51 +47,45 @@ function Login() {
     };
 
     return (
-        <div className="login-container" style={{ backgroundImage: `url(${court})` }}>
-          <div className="login-box">
-            <h1>Welcome Back</h1>
-            <p>Log into your Account</p>
-            <form onSubmit={handleSubmit} className="login-form">
-            <label htmlFor="email-address" className="login-label">Email Address</label>
-              <div className="input-group">
-                <FaEnvelope className="input-icon" />
-                <input
-                  type="email"
-                  id="email-address"
-                  name="email"
-                  placeholder="Email Address"
-                  onChange={handleInput}
-                  value={values.email}
-                  className="input-field"
-                />
-                {errors.email && <p className="error below-password">{errors.email}</p>}
-                </div>
-                <label htmlFor="password" className="login-label">Password</label>
-                <div className="input-group">
-                <FaLock className="input-icon" />
-                
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="Password"
-                    onChange={handleInput}
-                    value={values.password}
-                    className="input-field"
-                    />
-                    {errors.password && <p className="
-                        error below-password">{errors.password}</p>}
-                        </div>
-                        {errors.password<0 && errors.email <0 && backendError.length > 0 && (
-                        <p className="error below-password">{backendError[0]}</p>
-                        )}
-                        <button type="submit" className="login-btn">Login</button>
-                        <a href="#" className="forgot-password">Forgot password?</a>
-                        </form>
-                </div>
-        
+        <div className="login-container">
+            <div className="logo-container">
+                <img src={logo} alt="myHalal Checker Logo" className="logo" />
+                <h1>myHalal Checker</h1>
             </div>
-      );
+            <div className="login-box">
+                <h1 className="welcome">Welcome Back</h1>
+                <form onSubmit={handleSubmit} className="login-form">
+                    <div className="input-group">
+                        <FaEnvelope className="input-icon" />
+                        <input
+                            type="email"
+                            id="email-address"
+                            name="email"
+                            onChange={handleInput}
+                            value={values.email}
+                            className="input-field"
+                            placeholder="Email Address" />
+                        {errors.email && <p className="error">{errors.email}</p>}
+                    </div>
+                    <div className="input-group">
+                        <FaLock className="input-icon" />
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            onChange={handleInput}
+                            value={values.password}
+                            className="input-field"
+                            placeholder="Password" />
+                        {errors.password && <p className="error">{errors.password}</p>}
+                    </div>
+                    {backendError.length > 0 && <p className="error">{backendError[0]}</p>}
+                    <button type="submit" className="login-btn">Login</button>
+                </form>
+                <a href="" className="forgot-password">Forgot Password?</a>
+            </div>
+        </div>
+    );
 }
 
 export default Login;

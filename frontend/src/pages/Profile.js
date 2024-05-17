d5496acd70579c2c872b5aab642e7c5b70eada01
Sui Yee <yeeng2001@gmail.com>
2024-05-17 16:55:47 +0800

    profile ui update

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -49,17 +49,17 @@ const PasswordChangeModal = ({ isOpen, onClose, onSave }) => {
     );
   };
 
-const Profile = () => {
-    const [profileData, setProfileData] = useState({ username: '', email: '', role: '', gender: '', age: '', phone: '' });
-    const [editingField, setEditingField] = useState(null); // Track which field is being edited
+  const Profile = () => {
+    const [profileData, setProfileData] = useState({
+        username: '', email: '', role: '', gender: '', age: '', phone: ''
+    });
+    const [editedData, setEditedData] = useState({});
     const userID = localStorage.getItem('userID');
     const [isPasswordModalOpen, setIsPasswordModalOpen] = useState(false);
 
     useEffect(() => {
         if (userID) {
             fetchUserProfile(userID);
-        } else {
-            console.error('User ID not found in localStorage');
         }
     }, [userID]);
 
@@ -67,16 +67,13 @@ const Profile = () => {
         axios.get(`http://localhost:8085/userProfile/${userID}`)
             .then(response => {
                 setProfileData(response.data);
+                setEditedData(response.data); // Initialize edited data with fetched data
             })
             .catch(error => console.error("Error fetching user profile:", error));
     };
 
-    const handleEditClick = (fieldName) => {
-        setEditingField(fieldName);
-    };
-
-    const handleCancelClick = () => {
-        setEditingField(null);
+    const handleChange = (e) => {
+        setEditedData({ ...editedData, [e.target.name]: e.target.value });
     };
 
     const handlePasswordChange = (newPassword) => {
@@ -93,81 +90,63 @@ const Profile = () => {
         });
     };
 
-    const handleSaveClick = async (fieldName) => {
+    const handleSaveChanges = async () => {
         try {
-          const response = await axios.put(`http://localhost:8085/updateUserProfile/${userID}`, {
-            [fieldName]: profileData[fieldName]
-          });
-          console.log(response.data);
-          // If update was successful, reset the editing field to null
-          setEditingField(null);
+            const response = await axios.put(`http://localhost:8085/updateUserProfile/${userID}`, editedData);
+            setProfileData(editedData); // Update the displayed data
+            alert('Profile updated successfully!');
         } catch (error) {
-          console.error("Error updating user profile:", error);
+            console.error("Error updating user profile:", error);
         }
-      };
-      
-
-    const handleChange = (e) => {
-        setProfileData({ ...profileData, [e.target.name]: e.target.value });
     };
 
-    const renderField = (fieldKey, isEditable) => {
-        return (
-            <div key={fieldKey} className="profile-field">
-                <div className="field-title">
-                    <strong>{fieldKey.charAt(0).toUpperCase() + fieldKey.slice(1)}</strong>
-                </div>
-                <div className="field-content">
-                    {editingField === fieldKey ? (
-                        <>
-                            <input
-                                type="text"
-                                name={fieldKey}
-                                value={profileData[fieldKey]}
-                                onChange={handleChange}
-                            />
-                            <FaCheck className="icon save-icon" onClick={() => handleSaveClick(fieldKey)} />
-                            <FaTimes className="icon cancel-icon" onClick={handleCancelClick} />
-                        </>
-                    ) : (
-                        <>
-                            <span>{profileData[fieldKey]}</span>
-                            {isEditable && <FaPencilAlt className="icon edit-icon" onClick={() => handleEditClick(fieldKey)} />}
-                        </>
-                    )}
-                </div>
-            </div>
-        );
-    };
-    
+
+    const renderField = (fieldKey, label, isEditable) => (
+        <div className="field-row">
+            <label>{label}</label>
+            <input
+                type="text"
+                name={fieldKey}
+                value={editedData[fieldKey] || ''}
+                onChange={handleChange}
+                readOnly={!isEditable}
+                style={!isEditable ? { backgroundColor: '#f3f3f3', color: '#ccc' } : {}}
+            />
+        </div>
+    );
+
     return (
-        <div className="page-wrapper">
-            <div className="profile-container">
-                <div className="avatar-container">
-                    <img
-                        src={`http://localhost:8085/images/userProfile/${profileData.imageURL}`}
-                        alt="Profile"
-                        className="avatar"
-                        style={{ width: '100px', height: 'auto' }}
-                    />
-                    <div className="user-info">
-                        {renderField('username', true)}
-                        {renderField('email', true)}
-                        {renderField('role', false)}
-                        {renderField('gender', true)}
-                        {renderField('age', true)}
-                        {renderField('phone', true)}
-                    </div>
-                    <button onClick={() => setIsPasswordModalOpen(true)} className="change-password-btn">CHANGE PASSWORD</button>
-                    <PasswordChangeModal
-                        isOpen={isPasswordModalOpen}
-                        onClose={() => setIsPasswordModalOpen(false)}
-                        onSave={handlePasswordChange}
-                    />
-                </div>
+        <div className="profile-container">
+        <h1 className="profile-header">Profile</h1>
+            <div className="profileHeader">
+            <img
+                src={`http://localhost:8085/images/userProfile/${profileData.imageURL}`}
+                alt="Profile"
+                className="avatar"
+                style={{ width: '100px', height: 'auto' }}
+            />
             </div>
+            <div className="profile-fields">
+                {renderField('username', 'Username', true)}
+                {renderField('email', 'Email', true)}
+                {renderField('role', 'Role', false)}
+                {renderField('gender', 'Gender', false)}
+                {renderField('age', 'Age', true)}
+                {renderField('phone', 'Phone Number', true)}
+            </div>
+            <div className="save-changes">
+                <button className="save-changes-btn" onClick={handleSaveChanges}>Save Changes</button>
+                <button onClick={() => setIsPasswordModalOpen(true)} className="change-password-btn">Change Password</button>
+            </div>
+            <PasswordChangeModal
+                isOpen={isPasswordModalOpen}
+                onClose={() => setIsPasswordModalOpen(false)}
+                onSave={handlePasswordChange}
+            />
         </div>
     );
 };
+
+
 
 export default Profile;
